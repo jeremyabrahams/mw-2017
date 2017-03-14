22041c8edded88538c26dce3587f22862a8650ee
Jeremy Abrahams <jeremy@mightyinthemidwest.com>
2017-03-14 09:29:53 -0400

    Gulp Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp 		= require('gulp');
+var sass 		= require('gulp-sass');
+var minifycss	= require('gulp-minify-css');
+var browserSync = require('browser-sync').create();
 
+// Sass Task
 gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('assets/css'));
+		.pipe(minifycss())
+        .pipe(gulp.dest('assets/css'))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
+// BrowserSync Server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// Default
+gulp.task('default', ['sass', 'browser-sync'], function() {
     gulp.watch(['src/scss/*', 'src/scss/**/*'], ['sass']);
+	gulp.watch('*.html').on('change', browserSync.reload);
 });
